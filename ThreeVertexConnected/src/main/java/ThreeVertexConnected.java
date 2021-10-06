7383e08b924865c8f5f68be4fa07806d8b89f3ff
Никита Осипцов <osipczovnikita@gmail.com>
2021-10-06 18:13:58 +0300

    structured the project

---
+++ ThreeVertexConnected/src/main/java/ThreeVertexConnected.java
@@ -0,0 +1,10 @@
+import com.mathsystem.graphapi.AbstractGraph;
+import com.mathsystem.plugin.GraphProperty;
+
+public class ThreeVertexConnected implements GraphProperty {
+    @Override
+    // Реализовать
+    public boolean execute(AbstractGraph abstractGraph) {
+        return false;
+    }
+}
