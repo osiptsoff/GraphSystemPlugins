5e670aa708ecfd2871c442febdbe57f59668249b
Марина Петракова <mrpetrakova2000@gmail.com>
2021-10-10 00:10:36 +0300

    new branch LinearOrderingRelation

---
+++ LinearOrderingRelation/src/main/java/LinearOrderingRelation.java
@@ -0,0 +1,11 @@
+import com.mathsystem.graphapi.AbstractGraph;
+import com.mathsystem.plugin.GraphProperty;
+
+public class LinearOrderingRelation implements GraphProperty {
+    @Override
+    // Реализовать
+    public boolean execute(AbstractGraph abstractGraph) {
+        return false;
+    }
+}
+
