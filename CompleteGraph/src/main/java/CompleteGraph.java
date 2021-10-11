245636efe0ae11ea4fc9d90cafff08edd42608cb
stepanshvets <sh_stepa@mail.ru>
2021-10-11 12:22:32 +0300

    Add module CompleteGraph

---
+++ CompleteGraph/src/main/java/CompleteGraph.java
@@ -0,0 +1,9 @@
+import com.mathsystem.graphapi.AbstractGraph;
+import com.mathsystem.plugin.GraphProperty;
+
+public class CompleteGraph implements GraphProperty {
+    @Override
+    public boolean execute(AbstractGraph abstractGraph) {
+        return false;
+    }
+}
