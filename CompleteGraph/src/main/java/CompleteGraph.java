673ac6a8d2067950e77531e2e881fe0c607f1be0
stepanshvets <sh_stepa@mail.ru>
2021-10-30 12:25:56 +0300

    Add module CompleteGraph

--- CompleteGraph/src/main/java/CompleteGraph.java
+++ CompleteGraph/src/main/java/CompleteGraph.java
@@ -1,9 +1,24 @@
 import com.mathsystem.graphapi.AbstractGraph;
 import com.mathsystem.plugin.GraphProperty;
 
+import java.util.HashSet;
+
 public class CompleteGraph implements GraphProperty {
     @Override
     public boolean execute(AbstractGraph abstractGraph) {
-        return (abstractGraph.getVertexCount() * (abstractGraph.getVertexCount() - 1) / 2) == abstractGraph.getEdgeCount();
+        var vertexList = abstractGraph.getVertices();
+        var vertexCount = abstractGraph.getVertexCount();
+        for(int i = 0; i < vertexCount; i++){
+            var tmpVertex = vertexList.get(i);
+            var edgeList = tmpVertex.getEdgeList();
+            var adjVertex = new HashSet<Integer>();
+            for(int j = 0; j < edgeList.size(); j++){
+                var finalVertex = edgeList.get(j).getV();
+                if (edgeList.get(j).getV().getIndex() == tmpVertex.getIndex()) finalVertex = edgeList.get(j).getW();
+                adjVertex.add(finalVertex.getIndex());
+            }
+            if (adjVertex.size() < vertexCount - 1) return false;
+        }
+        return true;
     }
 }
